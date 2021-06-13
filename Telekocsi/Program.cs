9d8ccd22055474af65c775110dedf1d7b64edf6b
zeon77 <zeon77@gmail.com>
2021-06-13 14:33:50 +0200

    Add project files.

---
+++ Telekocsi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Telekocsi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
